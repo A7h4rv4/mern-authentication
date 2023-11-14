ab3597072cb1895850e01d9cc733f31ceada4fda
A7h4rv4 <132071926+A7h4rv4@users.noreply.github.com>
2023-11-14 21:56:59 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const port = 3000;
@@ -16,9 +17,11 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server live on port ${port}...`);
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    email: { type: email, required: true, unique: true },
-    password: { type: password, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
