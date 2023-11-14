ee2fb92b692aea3282f5b6d523d402888f09cf86
A7h4rv4 <132071926+A7h4rv4@users.noreply.github.com>
2023-11-14 20:48:52 +0530

    MongoDb Connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const port = 3000;
 
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(port, () => {
